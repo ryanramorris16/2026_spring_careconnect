c92814b3ec81767a3b56b88ed935e0e9a92a61cb
gjurado1 <gary.jurado1@outlook.com>
2026-02-26 13:31:22 +0100

    Updated conditional startup import in local_db_startup.dart

--- frontend/lib/services/local_db/local_db_startup.dart
+++ frontend/lib/services/local_db/local_db_startup.dart
@@ -1,4 +1,5 @@
 import 'local_db_startup_stub.dart'
+    if (dart.library.html) 'local_db_startup_web.dart'
     if (dart.library.io) 'local_db_startup_io.dart' as impl;
 
 /// Platform-aware startup hook for local DB initialization.
