74e617bc02e50dbc29d3e0eeb1fc2d9265f948d5
stinkyBootsLLC <stinkybootsllc@gmail.com>
2026-03-15 14:44:31 -0400

    test: deprecated framework
    replaced with new version

--- backend/core/src/test/java/com/careconnect/controller/dev/DevTelemetryControllerTest.java
+++ backend/core/src/test/java/com/careconnect/controller/dev/DevTelemetryControllerTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,10 +41,10 @@ class DevTelemetryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private TelemetryService telemetryService;
 
-    @MockBean
+    @MockitoBean
     private TelemetryToggleService telemetryToggleService;
 
     @Test
